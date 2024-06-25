62b3b7ff581c8ccb9847d86fecd3835f9291ddf8
Davyd Tseluiko <tseluiko.davyd@gmail.com>
2024-06-25 12:36:22 -0500

    Complete fourth exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...elements) {
+  for (element of elements) {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] === element) {
+        array.splice(i, 1);
+        i--;
+      }
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
