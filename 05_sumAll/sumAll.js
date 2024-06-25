a5b94a9a2e7eae208b0cab04385bd3fe32a180fe
Davyd Tseluiko <tseluiko.davyd@gmail.com>
2024-06-25 12:52:48 -0500

    Complete fifth exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function (x, y) {
+  if (typeof x !== "number" || typeof y !== "number" || x < 0 || y < 0) {
+    return "ERROR";
+  }
 
+  let sum = 0;
+  if (x < y) {
+    while (x <= y) sum += x++;
+  } else {
+    while (x >= y) sum += y++;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
