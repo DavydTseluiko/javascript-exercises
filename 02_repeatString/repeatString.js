757338b4af3cfc8857d2cef4864ebb108d6710ee
Davyd Tseluiko <tseluiko.davyd@gmail.com>
2024-06-25 11:50:52 -0500

    Complete second exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function (string, n) {
+  let stringRepeated = "";
+  for (let i = 0; i < n; i++) {
+    stringRepeated += string;
+  }
 
+  if (n < 0) {
+    return "ERROR";
+  } else {
+    return stringRepeated;
+  }
 };
 
 // Do not edit below this line
