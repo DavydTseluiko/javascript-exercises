db6ab91ac5cb369a38e08293ae2484c445514b38
Davyd Tseluiko <tseluiko.davyd@gmail.com>
2024-06-29 14:44:10 -0500

    Complete eighth exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  if (array.length === 0) {
+    return 0;
+  }
+  return array.reduce((total, number) => total + number);
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce((total, number) => total * number, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  let countFactorial = 1;
+  for (let i = a; i > 0; i--) {
+    countFactorial *= i;
+  }
+
+  return countFactorial;
 };
 
 // Do not edit below this line
@@ -29,5 +37,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
