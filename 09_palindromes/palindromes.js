a48be05c60aa56c65d5f9d0d76c9617913ff4f64
Davyd Tseluiko <tseluiko.davyd@gmail.com>
2024-06-29 16:06:25 -0500

    Complete ninth exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (text) {
+  text = text.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~() ]/g, "");
+  copyText = text.split("").reverse().join("");
 
+  return text === copyText;
 };
 
 // Do not edit below this line
